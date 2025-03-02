dc5278a555ac1128d25c609ab3544a7860e04d26
Prasad <prasadjadhav510@gmail.com>
2025-03-02 14:43:49 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client"
+import express from "express"
+
+const app = express()
+
+const client = new PrismaClient()
+
+app.get("/", async (req,res)=>{
+    const users = await client.user.findMany()
+
+    res.json({
+        status:"supper success",
+        users
+    })
+})
+
+app .post("/",async (req,res)=>{
+
+    await client.user.create({
+        data:{
+            username:Math.random().toString(),
+            password:Math.random().toString()
+        }
+    })
+
+    res.send("user created sucessfully")
+
+})
+
+app.listen(3000)
